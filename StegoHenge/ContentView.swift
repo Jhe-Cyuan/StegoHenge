74b2aa115f037895bb8626b1e9163a956a9769ff
JCYang <yang880519@gmail.com>
2020-10-20 00:44:21 +0800

    2020/10/20 00:33
    Add Function: Core Data for Configure of Application.
    Now cyptography and steganography algorithm user chose can be remembered.
    
    Add UI: Add RSA Key-Manager in Setting-Page.
    Long tap gesture will show context menu with "Edit" and "Delete" button, but unfunctional.

--- StegoHenge/ContentView.swift
+++ StegoHenge/ContentView.swift
@@ -11,8 +11,10 @@ struct ContentView: View {
     
     @State private var bShowSetting:Bool = false
     
-    @State private var strStegoAlgo:String = "LSB Original"
-    @State private var strCyptoAlgo:String = "RSA"
+    @State private var strStegoAlgo:String = ""
+    @State private var strCyptoAlgo:String = ""
+    
+    @State private var textsRSAKey:[Text] = [Text]()
     
     var body: some View {
         NavigationView {
@@ -59,6 +61,8 @@ struct ContentView: View {
                     
                     Form {
                         VStack {
+                            Spacer()
+                            
                             Text("Cryptography Algorithm")
                                 .font(.custom("Futura-Medium", size: 20))
                             
@@ -67,9 +71,13 @@ struct ContentView: View {
                                 Text("RSA").tag("RSA")
                             }
                             .pickerStyle(SegmentedPickerStyle())
+                            
+                            Spacer()
                         }
                         
                         VStack {
+                            Spacer()
+                            
                             Text("Steganography Algorithm")
                                 .font(.custom("Futura-Medium", size: 20))
                             
@@ -77,41 +85,119 @@ struct ContentView: View {
                                 Text("LSB Original")
                                     .tag("LSB Original")
                                 /*Text("LSB Match")
-                                    .tag("LSB Match")*/
+                                 .tag("LSB Match")*/
                                 Text("F5 Algorithm")
                                     .tag("F5 Algorithm")
                             }
                             .pickerStyle(SegmentedPickerStyle())
+                            
+                            Spacer()
                         }
                         
-                        HStack {
+                        VStack {
                             Spacer()
                             
-                            VStack {
-                                Group {
-                                    Text("Leader")
-                                    Text("06360293 楊哲銓")
-                                    Text("Member")
-                                    Text("06360870 曾志敏")
-                                    Text("06361155 馬叡竣")
-                                    Text("Teacher")
-                                    Text("MCU CSIE 李遠坤副教授")
+                            HStack {
+                                Spacer()
+                                
+                                Text("RSA Key")
+                                
+                                Button(action: {print("press")}) {
+                                    Image(systemName: "person.crop.circle.badge.plus")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 40)
                                 }
-                                Text("")
-                                Group {
-                                    Text("Copyright © 2020")
-                                    Text("MCU Graduation Project By Team 4.")
-                                    Text("All rights reserved.")
+                                .buttonStyle(BorderlessButtonStyle())
+                                
+                                Spacer()
+                            }
+                            
+                            Form {
+                                ForEach(0..<self.textsRSAKey.count) {
+                                    self.textsRSAKey[$0]
+                                        .contextMenu{
+                                            Button(action:{}) {
+                                                Text("Edit")
+                                                Image(systemName: "pencil")
+                                            }
+                                            Button(action:{}) {
+                                                Text("Delete")
+                                                    .foregroundColor(.red)
+                                                Image(systemName: "trash")
+                                                    .foregroundColor(.red)
+                                            }
+                                        }
                                 }
                             }
-                            .font(.custom("Futura-Medium", size: 16))
+                            .frame(height: 200)
                             
+                            Spacer()
+                        }
+                        .font(.custom("Futura-Medium", size: 20))
+                        
+                        VStack {
+                            Spacer()
+                            HStack {
+                                Spacer()
+                                
+                                VStack {
+                                    Group {
+                                        Text("Leader")
+                                        Text("06360293 楊哲銓")
+                                        Text("Member")
+                                        Text("06360870 曾志敏")
+                                        Text("06361155 馬叡竣")
+                                        Text("Teacher")
+                                        Text("MCU CSIE 李遠坤副教授")
+                                    }
+                                    Text("")
+                                    Group {
+                                        Text("Copyright © 2020")
+                                        Text("MCU Graduation Project By Team 4.")
+                                        Text("All rights reserved.")
+                                    }
+                                }
+                                .font(.custom("Futura-Medium", size: 16))
+                                
+                                Spacer()
+                            }
                             Spacer()
                         }
                     }
+                    .onAppear() {
+                        let app = UIApplication.shared.delegate as! AppDelegate
+                        let context = app.persistentContainer.viewContext
+                        var config:[Configure]
+                        
+                        do {
+                            config = try context.fetch(Configure.fetchRequest())
+                            
+                            self.strCyptoAlgo = config[0].strCyptoAlgo!
+                            self.strStegoAlgo = config[0].strStegoAlgo!
+                        } catch {
+                            print(error)
+                        }
+                    }
                     
-                    Button(action: {self.bShowSetting = false}) {
+                    Button(action: {
+                        self.bShowSetting = false
+                        
+                        let app = UIApplication.shared.delegate as! AppDelegate
+                        let context = app.persistentContainer.viewContext
+                        var config:[Configure]
+                        
+                        do {
+                            config = try context.fetch(Configure.fetchRequest())
+                            config[0].strCyptoAlgo = self.strCyptoAlgo
+                            config[0].strStegoAlgo = self.strStegoAlgo
+                        } catch {
+                            print(error)
+                        }
+                        app.saveContext()
+                    }) {
                         Text("OK")
+                            .frame(width: UIScreen.main.bounds.width)
                     }
                     .padding(.all, 20)
                 })
@@ -127,6 +213,30 @@ struct ContentView: View {
                     }
                     .frame(width: 100, height: 100, alignment: .trailing)
             )
+            .onAppear() {
+                let app = UIApplication.shared.delegate as! AppDelegate
+                let context = app.persistentContainer.viewContext
+                var config:[Configure]
+                
+                do {
+                    config = try context.fetch(Configure.fetchRequest())
+                    
+                    switch config.count {
+                    case 0:
+                        let newConfig = Configure(context: context)
+                        newConfig.strCyptoAlgo = "RSA"
+                        newConfig.strStegoAlgo = "LSB Original"
+                        self.strCyptoAlgo = "RSA"
+                        self.strStegoAlgo = "LSB Original"
+                        app.saveContext()
+                    default:
+                        self.strCyptoAlgo = config[0].strCyptoAlgo!
+                        self.strStegoAlgo = config[0].strStegoAlgo!
+                    }
+                } catch {
+                    print(error)
+                }
+            }
         }
     }
 }
